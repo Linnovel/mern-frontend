0b0a59c0d7999908aab3e6e55d09de61c83dbca5
linnovel <luislinnovel@gmail.com>
2025-05-10 22:58:51 -0400

    vamos maldito render

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000"
-      }
-    },
-    port: process.env.PORT || 4173,  // Asegúrate de que Vite use el puerto proporcionado por Render.
-  }
+  plugins: [react()]
 })
